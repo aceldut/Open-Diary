14eded904883b70c011344f2ae185d3d0f30e7bc
Marcell <marcellioach@gmail.com>
2024-04-20 12:26:32 +0700

    After Sign Out

---
+++ components/global/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from "next/link";
+import NavbarButton from './NavbarButton';
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="container flex mx-auto justify-between p-4">
+                <Link href='/' className="btn btn-ghost text-xl">
+                    OpenDairy
+                </Link>
+                <NavbarButton />
+            </div>
+        </div>
+    )
+}
+export default Navbar;
